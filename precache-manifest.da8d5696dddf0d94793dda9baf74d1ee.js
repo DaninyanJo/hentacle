686c1c8c250169bdf7935f0053a89a15bf822e19
DaninyanJo <daninyanJo@github.com>
2020-07-03 09:01:05 +0300

    Updates

---
+++ precache-manifest.da8d5696dddf0d94793dda9baf74d1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35613405cce196f658577adcec8dd50e",
+    "url": "/hentacle/index.html"
+  },
+  {
+    "revision": "c22fdebf237f1f921c5b",
+    "url": "/hentacle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "735859147046109a85c5",
+    "url": "/hentacle/static/js/2.8f160f27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hentacle/static/js/2.8f160f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22fdebf237f1f921c5b",
+    "url": "/hentacle/static/js/main.2cf0244f.chunk.js"
+  },
+  {
+    "revision": "ec9b614611ff2afa6943",
+    "url": "/hentacle/static/js/runtime-main.c05f0cbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hentacle/static/media/logo.5d5d9eef.svg"
+  }
+]);
